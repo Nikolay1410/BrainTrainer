44e9b2f1a4730a6465f04adc0b1de83b42df4fa2
Николай Таран <66410612+Nikolay1410@users.noreply.github.com>
2022-09-01 10:01:43 +0300

    Version: 1.4 Проведина оптимизация

--- app/src/main/java/com/example/braintrainer/ActivityGame.java
+++ app/src/main/java/com/example/braintrainer/ActivityGame.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
